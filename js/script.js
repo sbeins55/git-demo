6502c820e4bc01d999d645179e9941eeea3cf80c
Sean Beins <sean.beins@gmail.com>
2017-04-12 19:26:24 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-// Only change code below this line.
 $(document).ready(function(){
-    if (navigator.geolocation) {
-      $("#location").on("click", function(){
-           navigator.geolocation.getCurrentPosition(function(position){
-          $("#message").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+    
+    // When the location button is clicked then display current position in the message well.
+    $("#location").on("click", function(){
+        navigator.geolocation.getCurrentPosition(function(position){
+            $("#message").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
         });
-      });
-    }
+    });
+
+
 });
